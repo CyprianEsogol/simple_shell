992a6921edd3bfe2cd7d5d253bfb8bcc7f86124f
Mabende <belindamabende@gmail.com>
2023-07-29 09:12:53 -0700

    updated

--- forkexe.c
+++ forkexe.c
@@ -5,34 +5,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 /**
- * forkexe - System execution with fork
- * @arstr: Array of strings gotten from line command
- * @envp: Environment variables from the parent
- * Return: void
- */
-void forkexe(char **arstr, char *envp[])
-{
-	pid_t child_pid;
-	int status;
-
-	child_pid = fork();
-	if (child_pid < 0)
-	{
-		perror("Error: Fork issue");
-		return;
-	}
-	if (child_pid == 0)
-	{
-		if (execve(arstr[0], arstr, envp) == -1)
-			perror("./shell: No such file or directory\n");
-	}
-	else
-	{
-		wait(&status);
-	}
-}
-#include "main.h"
-/**
  * execute - Execute a command with arguments
  *
  * @args: Array of strings containing command and arguments
