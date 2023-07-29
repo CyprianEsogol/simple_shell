642c9034e7b736b16320968e474e777fd739fb09
Mabende <belindamabende@gmail.com>
2023-07-29 07:53:10 -0700

    updated

---
+++ path.c
@@ -0,0 +1,83 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_COMMAND_LENGTH 100
+#define MAX_PATH_LENGTH 200
+
+/**
+ * get_command_path - Get the full path of a command using the PATH variable
+ * @command: Command to find
+ * Return: Full path of the command if found, NULL otherwise
+ */
+char *get_command_path(char *command)
+{
+	char *path, *dir, *full_path, *token;
+	char *slash_ptr;
+
+	if (_strchr(command, '/') != NULL)
+	{
+		return (_strdup(command));
+	}
+
+	path = _getenv("PATH");
+	if (path == NULL)
+	{
+		fprintf(stderr, "Error retrieving PATH variable\n");
+		return (NULL);
+	}
+
+	token = strtok(path, ":");
+	while (token != NULL)
+	{
+		dir = _strdup(token);
+		full_path = (char *)malloc(_strlen(dir) + _strlen(command) + 2);
+		if (full_path == NULL)
+		{
+			fprintf(stderr, "Allocation error\n");
+			free(dir);
+			return (NULL);
+		}
+		_strcpy(full_path, dir);
+		_strcat(full_path, "/");
+		_strcat(full_path, command);
+
+		if (access(full_path, X_OK) == 0)
+		{
+			free(dir);
+			return (full_path);
+		}
+		free(full_path);
+		free(dir);
+		token = strtok(NULL, ":");
+	}
+	return (NULL);
+}
+
+/**
+ * build_command_path - Build the full path of a command
+ * @directory: Directory to prepend to the command
+ * @command: Command to append to the directory
+ * Return: Full path of the command
+ */
+char *build_command_path(char *directory, char *command)
+{
+	size_t directory_len = _strlen(directory);
+	size_t command_len = _strlen(command);
+	size_t path_len = directory_len + command_len + 2;
+	char *path = (char *)malloc(path_len);
+
+	if (path == NULL)
+	{
+		fprintf(stderr, "Allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	_strncpy(path, directory, directory_len);
+	path[directory_len] = '/';
+	_strncpy(path + directory_len + 1, command, command_len);
+	path[path_len - 1] = '\0';
+	return (path);
+}
+
