d421dc376ff51bdb2c3a2bd649eb28fe8664a758
Mabende <belindamabende@gmail.com>
2023-07-29 06:55:31 -0700

    updated

--- main.h
+++ main.h
@@ -35,7 +35,7 @@ int _strcmp(char *fi, char *sd);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 char *filechk(char *str)
 int _putchar(char c)
-size_t _strlen(const char *s)
+ssize_t _strlen(const char *s)
 void free_args(char **args)
 char *_strncpy(char *dest, const char *src, size_t n)
 char *_strcpy(char *destination, const char *source)
